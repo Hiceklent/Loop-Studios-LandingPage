4a0efe28ac392ad2e82b00b81d3f814c288e7171
María Tamayo <cielitoazul.1027@gmail.com>
2024-08-10 21:34:22 -0500

    :white_check_mark: [Finish] Header-finalizado

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -3,9 +3,27 @@ import './header.scss'
 const header = () => {
   return (
     <>
-    <header>
-      <p>hola perrasen header</p>
-    </header>
+      <header className="header">
+        <div className="logo">loopstudios</div>
+        <nav>
+          <ul>
+            <li><a href="#about">About</a></li>
+            <li><a href="#careers">Careers</a></li>
+            <li><a href="#events">Events</a></li>
+            <li><a href="#products">Products</a></li>
+            <li><a href="#support">Support</a></li>
+          </ul>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg"
+              height="24"
+              viewBox="0 -960 960 960"
+              width="24"
+              fill="#FFF">
+              <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+            </svg>
+          </button>
+        </nav>
+      </header>
     </>
   )
 }
