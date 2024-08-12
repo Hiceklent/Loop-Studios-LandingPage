bf5731cc1f931788790321d2896ea54d52a8d9e5
Holguer Pinzon <93726097+Hiceklent@users.noreply.github.com>
2024-08-12 16:26:30 -0500

    🐛 [FIX] Fix Footer and Header

--- src/components/common/footer/footer.jsx
+++ src/components/common/footer/footer.jsx
@@ -1,35 +1,33 @@
-// import FacebookIcon from '../../../assets/icons/icon-facebook.svg';
-// import TwitterIcon from '../../../assets/icons/icon-twitter.svg';
-// import PinterestIcon from '../../../assets/icons/icon-pinterest.svg';
-// import InstagramIcon from '../../../assets/icons/icon-Instagram.svg';
-/*import './Footer.scss';*/
+import './Footer.scss';
+import FacebookIcon from '../../../assets/icons/icon-facebook.svg';
+import TwitterIcon from '../../../assets/icons/icon-twitter.svg';
+import PinterestIcon from '../../../assets/icons/icon-pinterest.svg';
+import InstagramIcon from '../../../assets/icons/icon-Instagram.svg';
 
 const Footer = () => {
   return (
-
-      <Footer>
-        <div className="logo">loopstudios</div>
-        <nav>
-          <ul>
-            <li><a href="#about">About</a></li>
-            <li><a href="#careers">Careers</a></li>
-            <li><a href="#events">Events</a></li>
-            <li><a href="#products">Products</a></li>
-            <li><a href="#support">Support</a></li>
-          </ul>
-        </nav>
-        <div className="footer-social">
-            <div className="social-icons">
-              {/* <img src={FacebookIcon} alt="Facebook" />
-              <img src={TwitterIcon} alt="Twitter" />
-              <img src={PinterestIcon} alt="Pinterest" />
-              <img src={InstagramIcon} alt="Instagram" /> */}
-            </div>
-          </div>
-        <p className="footer-bottom"> &copy; 2023 Loopstudios. All rights reserved.</p>
-      </Footer>
-
-  );
-};
+    <footer>
+      <div className="logo">loopstudios</div>
+      <nav>
+        <ul>
+          <li><a href="">About</a></li>
+          <li><a href="">Careers</a></li>
+          <li><a href="">Events</a></li>
+          <li><a href="">Products</a></li>
+          <li><a href="">Support</a></li>
+        </ul>
+      </nav>
+      <div className="footer-social">
+        <div className="social-icons">
+          <img src='/src/assets/icons/icon-facebook.svg' alt="Facebook" />
+          <img src={TwitterIcon} alt="Twitter" />
+          <img src={PinterestIcon} alt="Pinterest" />
+          <img src={InstagramIcon} alt="Instagram" />
+        </div>
+      </div>
+      <p className="footer-bottom"> &copy; 2023 Loopstudios. All rights reserved.</p>
+    </footer>
+  )
+}
 
 export default Footer;
