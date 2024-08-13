b75aefdfc6479f57ea8bc2af579fb30240e2c324
María Tamayo <cielitoazul.1027@gmail.com>
2024-08-13 17:56:07 -0500

    🍱 [UPDATE] make-menu-hamburger

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -1,11 +1,18 @@
-import './Header.scss'
+import React, { useState } from 'react';
+import './Header.scss';
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <header className="header">
         <div className="logo">loopstudios</div>
-        <nav>
+        <nav className={isOpen ? 'open' : ''}>
           <ul>
             <li><a href="#about">About</a></li>
             <li><a href="#careers">Careers</a></li>
@@ -13,19 +20,21 @@ const Header = () => {
             <li><a href="#products">Products</a></li>
             <li><a href="#support">Support</a></li>
           </ul>
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg"
+          <button onClick={handleClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
               height="24"
               viewBox="0 -960 960 960"
               width="24"
-              fill="#FFF">
+              fill="#FFF"
+            >
               <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
             </svg>
           </button>
         </nav>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default header
+export default Header;
