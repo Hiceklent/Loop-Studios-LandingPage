6abf77204132e6a90ec1eafdf87781e83cac2624
María Tamayo <cielitoazul.1027@gmail.com>
2024-08-13 00:44:38 -0500

    🔐 [REFRESH] update-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Footer from './components/common/Footer/Footer'
-import Header from './components/common/Header/Header'
-import Home from './pages/Home/Home'
+import Footer from './components/common/footer/footer'
+import Header from './components/common/header/header'
+import Home from './pages/home/home'
 
 function App() {
 
